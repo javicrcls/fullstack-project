b8f72f48dd388a4e17119fd1b13f97a436d2e5eb
JaviC <javier.carcelesm@gmail.com>
2022-08-03 16:40:15 +0200

    First commit with prj structure

---
+++ part1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Title </h1>
+      <strong> Working </strong>
+        <div>
+          <p> Result here </p>
+        </div>
+        Hello!
+    </div>
+  );
+}
+
+export default App;
